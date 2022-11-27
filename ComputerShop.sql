60c8b965e6eca4370a5d9265791034194d3491d8
IronNicA <118505762+IronNicA@users.noreply.github.com>
2022-11-27 20:50:16 +0700

    Update ComputerShop.sql

--- ComputerShop.sql
+++ ComputerShop.sql
@@ -8,3 +8,12 @@ customerUsername varchar(30) not null,
 customerPassword varchar(30) not null,
 customerMail varchar(40)
 );
+create table Staffs
+(
+staffID smallint not null primary key,
+staffUsername varchar(30) not null,
+staffPassword varchar(30) not null,
+stafName varchar(40) not null,
+staffPhone int(10),
+staffEmail varchar(40)
+);
