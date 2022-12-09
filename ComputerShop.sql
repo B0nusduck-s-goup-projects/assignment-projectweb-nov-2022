2a7a64b16d043fde5aa3f37f7c367de782b826e6
B0nusduck <88030859+B0nusduck@users.noreply.github.com>
2022-12-09 19:39:51 +0700

    Update ComputerShop.sql

--- ComputerShop.sql
+++ ComputerShop.sql
@@ -13,7 +13,7 @@ create table staffs
 staffID smallint not null primary key,
 staffUsername varchar(30) not null,
 staffPassword varchar(30) not null,
-stafName varchar(40) not null,
+staffName varchar(40) not null,
 staffPhone int(10),
 staffEmail varchar(40)
 );
