f599abd630fdaed77db5e5469f7598023b905a95
B0nusduck <88030859+B0nusduck@users.noreply.github.com>
2022-12-12 13:41:39 +0700

    Update ComputerShop.sql

--- ComputerShop.sql
+++ ComputerShop.sql
@@ -17,3 +17,38 @@ staffName varchar(40) not null,
 staffPhone int(10),
 staffEmail varchar(40)
 );
+create table categories
+(
+categoryID smallint not null primary key,
+categoryName varchar(40)
+);
+create table products
+(
+productID mediumint not null primary key,
+categoryID smallint not null,
+productName varchar(100),
+productPrice double(15,3),
+productImage varchar(30),
+constraint FK_Cat_Pro foreign key (categoryID) references Categories(categoryID)
+);
+create table comments
+(
+productID mediumint not null,
+customerID int not null,
+commentStars smallint (5),
+commentContent varchar (500),
+constraint PK_Comments primary key (productID, customerID),
+constraint FK_Cus_Com foreign key (customerID) references Customers(customerID),
+constraint FK_Pro_Com foreign key (productID) references Products(productID)
+);
+create table carts
+(
+cartID int not null,
+productID mediumint not null,
+customerID int not null,
+productAmmount smallint,
+totalPrice double(15,4),
+constraint PK_Carts primary key (cartID, productID),
+constraint FK_Pro_Carts foreign key (productID) references Products(productID),
+constraint FK_Cus_Carts foreign key (customerID) references Customers(customerID)
+);
